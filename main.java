75f09f6f9319f69e0d114df6aee06db7dd96c5a2
Jeongumgu <33191329+Jeongumgu@users.noreply.github.com>
2023-11-20 11:02:50 +0900

    Delete main.java

--- main.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-import java.io.*;
-
-public class Main {
-    public void main (String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    }
-}
